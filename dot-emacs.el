b9d1ee0cd36032ca42f84c0796614b5568aea066
Christophe de Dinechin <christophe@dinechin.org>
2020-10-30 11:15:30 +0100

    Do not call package-initialize on emacs 27
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- dot-emacs.el
+++ dot-emacs.el
@@ -7,7 +7,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+(if (< emacs-major-version 27) (package-initialize))
 
 (cond
  ((and (string-match "XEmacs" emacs-version)
